548c51e2e826a66c0b3e1560f24bcf0009c64f68
Tackiee <takitt1720@gmail.com>
2024-03-02 23:37:01 +0900

    todo template

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyTodoApp());
+}
+
+class MyTodoApp extends StatelessWidget {
+  const MyTodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: todoListPage(),
+    );
+  }
+}
+
+class todoListPage extends StatelessWidget {
+  const todoListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatelessWidget {
+  const TodoAddPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面（クリックで戻る）'),
+        ),
+      ),
+    );
+  }
+}
